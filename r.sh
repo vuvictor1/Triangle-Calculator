c5525c87217d0a29ccb8ca666b501e23fbcfc916
Victor V Vu <78053016+vuvictor1@users.noreply.github.com>
2022-03-27 11:28:49 -0700

    Update r.sh

--- r.sh
+++ r.sh
@@ -6,6 +6,15 @@
 #Author: Victor V. Vu 
 #Section: Cpsc 240-07                                                                
 #Descrption: BASH compilation file
+#;********************************************************************************************
+#; Copyright (C) 2022 Victor V. Vu                                                           *
+#; This program is free software: you can redistribute it and/or modify it under the terms   * 
+#; of the GNU General Public License version 3 as published by the Free Software Foundation. * 
+#; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY  *
+#; without even the implied Warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. * 
+#; See the GNU General Public License for more details. A copy of the GNU General Public     *
+#; License v3 is available here:  <https://www.gnu.org/licenses/>.                           *                                                                                          
+#;********************************************************************************************
 
 #Removes old files when we create a new compilation
 rm *.o
